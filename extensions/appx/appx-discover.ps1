40e2f8b8b42b94ddc7fbf9e7ffe3153922e4871b
Steve Lee <slee@microsoft.com>
2025-10-17 15:59:08 -0700

    update appx extension for new extensions

--- extensions/appx/appx-discover.ps1
+++ extensions/appx/appx-discover.ps1
@@ -1,6 +1,17 @@
 $packages = Get-AppxPackage
+$extensions = @(
+    '*.dsc.adaptedresource.json'
+    '*.dsc.adaptedresource.yaml'
+    '*.dsc.adaptedresource.yml'
+    '*.dsc.manifests.json'
+    '*.dsc.manifests.yaml'
+    '*.dsc.manifests.yml'
+    '*.dsc.resource.json'
+    '*.dsc.resource.yaml'
+    '*.dsc.resource.yml'
+)
 foreach ($package in $packages) {
-    $manifests = Get-ChildItem -Path "$($package.InstallLocation)\*" -File -Include '*.dsc.resource.json','*.dsc.resource.yaml','*.dsc.resource.yml' -ErrorAction Ignore
+    $manifests = Get-ChildItem -Path "$($package.InstallLocation)\*" -File -Include $extensions -ErrorAction Ignore
     foreach ($manifest in $manifests) {
         @{ manifestPath = $manifest.FullName } | ConvertTo-Json -Compress
     }
